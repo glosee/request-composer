948c9bc9c15ed12c25bf663ec3269d3a4c9a3251
Graham <grahamlosee@gmail.com>
2020-09-28 16:48:16 -0400

    chore: add config files
    add package.json and other configuration files for other tools

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+const { NODE_ENV } = process.env;
+const targets =
+	NODE_ENV === 'test' ? { node: 'current' } : { browsers: ['last 2 versions'] };
+module.exports = {
+	presets: [['@babel/preset-env', { targets }]],
+};
